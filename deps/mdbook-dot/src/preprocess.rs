alek3y/cs

commit 78ef8b408a3a4a5ae5b7c830d1eb592e5f58b51e

Embed SVGs using data URIs

deps/mdbook-dot/src/preprocess.rs
@@ -1,3 +1,4 @@
+use base64::{engine::general_purpose, Engine};
 use mdbook::{
 	book::Book,
 	errors::Result,
@@ -40,21 +41,14 @@ impl Preprocessor for DotPreprocessor {
 	}
 }
 
-fn sanitize(svg: String) -> String {
-	let svg = Regex::new(r"<!DOCTYPE [^>]+>").unwrap().replace(&svg, "");
-	let svg = Regex::new(r"<\?xml [^>]+\?>").unwrap().replace(&svg, "");
-	let svg = Regex::new(r"\n").unwrap().replace_all(&svg, "");
-
-	svg.trim().into()
-}
-
 pub fn process_chapter(raw: String) -> String {
 	Regex::new(r"(?s)`{3}dot process\n(|.*?[^\\])`{3}")
 		.unwrap()
 		.replace_all(&raw, |caps: &Captures| {
 			let graph = caps.get(1).unwrap().as_str();
-			let svg = sanitize(render_dot(graph));
-			format!("<div>{svg}</div>\n")
+			let svg = general_purpose::STANDARD.encode(render_dot(graph));
+			let src = format!("data:image/svg+xml;base64,{svg}");
+			format!("<p><img src=\"{src}\"></p>")
 		})
 		.into()
 }

deps/mdbook-dot/Cargo.toml
@@ -8,3 +8,4 @@ mdbook = {version = "0.4.34", default-features = false}
 serde_json = "1.0"
 rayon = "1.8"
 regex = "1.9.6"
+base64 = "0.21.4"

commit 4cf6efae25b901cd6708413e595423177c535892

Fix formatting on text below dot blocks
Reapplies commit 182e8054ce7c2bb47abc09a76c976e0f99ac4037 after commit
78ef8b408a3a4a5ae5b7c830d1eb592e5f58b51e mistakenly reverted it

deps/mdbook-dot/src/preprocess.rs
@@ -48,7 +48,7 @@ pub fn process_chapter(raw: String) -> String {
 			let graph = caps.get(1).unwrap().as_str();
 			let svg = general_purpose::STANDARD.encode(render_dot(graph));
 			let src = format!("data:image/svg+xml;base64,{svg}");
-			format!("<p><img src=\"{src}\"></p>")
+			format!("<p><img src=\"{src}\"></p>\n")
 		})
 		.into()
 }